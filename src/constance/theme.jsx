449706ba94754dfb98a5066ecff26ab3d302c9d9
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-05-19 22:45:39 +0300

    стилі

--- src/constance/theme.jsx
+++ src/constance/theme.jsx
@@ -1,16 +1,16 @@
 export const theme = {
-    colors: {
-        background: '#454242',
-        text: '#c5c1c1',
-         accent: 'orange',
-        staticTitle: '#403e3e24',
-        isOnline: '#0a8a26',
-        isOffline: '#db1313',
-        friendName: '#0b0a0a',
-        transactionBg: ' #ffffff',
-        transactionColor: '#808184',
-        transactionBorder: '#b5b5b5',
-        tHeadBg: '#00bcd5',
-        tRow:'#ecf1f4',
-    }
-}
+  colors: {
+    background: '#4d30db',
+    text: '#d6c9c9',
+    accent: 'yellow',
+    staticTitle: '#403e3e24',
+    isOnline: '#0a8a26',
+    isOffline: '#db1313',
+    friendName: '#0b0a0a',
+    transactionBg: ' #ffffff',
+    transactionColor: '#808184',
+    transactionBorder: '#b5b5b5',
+    tHeadBg: '#00bcd5',
+    tRow: '#ecf1f4',
+  },
+};
